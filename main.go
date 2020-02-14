23fadbfb8ae12942a35571f2c83dc762b33b9975
David Kotalík <kotalik.dav@gmail.com>
2020-02-14 17:27:01 +0100

    Get table data.

--- main.go
+++ main.go
@@ -13,21 +13,20 @@ import (
 )
 
 var (
-
+  db *sql.DB
 )
 
 
 
-func dbInit(cfg Configuration) (*sql.DB) {
+func dbInit(cfg Configuration) {
 
   connString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", cfg.Database.User, cfg.Database.Pass, cfg.Database.Host, cfg.Database.Port, cfg.Database.Database)
 
-  db, err := sql.Open("mysql", connString)
+  var err error
+  db, err = sql.Open("mysql", connString)
   if err != nil {
     log.Fatal(err)
   }
-
-  return db
 }
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
@@ -43,6 +42,37 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 }
 
 func getTableData(w http.ResponseWriter, r *http.Request) {
+
+  vars := mux.Vars(r)
+  //dbName := vars["database"]
+  dbTable := vars["table"]
+
+  rows, err := db.Query("Select * from " + dbTable)
+  if err != nil {
+    panic(err.Error())
+  }
+  defer rows.Close()
+
+  columns, _ := rows.Columns()
+  dataMap := make([]map[string]interface{}, 0)
+
+  for rows.Next() {
+    colVals := make([]interface{}, len(columns))
+    colPtrs  := make([]interface{}, len(columns))
+    for i, _ := range colVals{
+      colPtrs[i] = &colVals[i]
+    }
+
+    if err := rows.Scan(colPtrs...); err != nil {
+      panic(err.Error())
+    }
+
+    rowMap := make(map[string]interface{})
+    for i, colName := range columns {
+      rowMap[colName] = *colPtrs[i].(*interface{})
+    }
+    dataMap = append(dataMap, rowMap)
+  }
 
 }
 
